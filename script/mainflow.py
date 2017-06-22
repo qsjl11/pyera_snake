6e5726cc6412534d2a894b97e0d5053182045ef2
qsjl11 <li_xin11@outlook.com>
2017-06-22 01:28:12 +0100

    could move and have basic menu

--- script/mainflow.py
+++ script/mainflow.py
@@ -1,95 +1,126 @@
 # -*- coding: UTF-8 -*-
 import core.game as game
-import script.saveload as saveload
-import script.lib as lib
-import random
+import script.base_lib as lib
 
 
-def test():
-    import math
-    game.p(math.exp(3))
-    return
-
-
-
-def open_func(*args):
+def open_func():
+    game.pl('pyera贪吃蛇游戏加载中')
+    map_size = game.data['map']['map_size']
+    game.pl('地图大小设置为 ' + str(map_size))
     game.pline()
-    game.pl('pyera 启动中，准备游戏')
-    game.def_style('notice', foreground='#DCE441')
-    game.def_style('title', foreground='#E1FFFF')
-    if not '人物列表' in game.data:
-        game.data['人物列表'] = []
-    if not '世界列表' in game.data:
-        game.data['世界列表'] = []
-    if not '队伍列表' in game.data:
-        game.data['队伍列表'] = []
-    import script.play_cfg
-    script.play_cfg.init()
-    game.pl(f'pyera 启动中，准备完成')
-    test()
-    open_menu()
 
-
-def open_menu():
-    # game.pline()
-    # game.pcmd('[001]  开始游戏', 1, newgame_func)
-    # game.pl()
-    # game.pcmd('[002]  读取游戏', 2, saveload.load_func, arg=(open_menu,))
-    # game.askfor_order()
-    game.clr_cmd()
-    game.pline()
-    game.pcmd('[001]  开始游戏', 1)
+    game.pcmd('[001]  读取游戏', 1, lib.load_func, arg=(open_func, main_func))
     game.pl()
-    game.pcmd('[002]  读取游戏', 2)
-
-    @game.set_deal_cmd_func_deco
-    def deal_func(order):
-        if order==1:
-            newgame_func()
-        if order==2:
-            saveload.load_func(open_menu)
+    game.pcmd('[002]  开始游戏', 2, newgame_func)
 
 
 def newgame_func():
-    game.pl()
-    game.pline()
-    game.pl('请输入主神名称:')
-    name = game.askfor_str()
-    game.data['主神名称'] = name
-    game.pl('主神名称为：' + str(name))
-    game.pl('是否接受?')
-    ans = lib.yes_or_no()
-    if ans == True:
-        main_func()
-    if ans == False:
-        newgame_func()
+    # 初始化地图数据dic，0是空地，1是苹果
+    map_size = game.data['map']['map_size']
+    game.data['mapdata'] = {}
+    for y in range(0, map_size):
+        for x in range(0, map_size):
+            game.data['mapdata'][(x, y)] = 0
 
+    # 初始化蛇list, 三格长度，靠左下角放置
+    snake_list = []
+    snake_list.append((0, 2))
+    snake_list.append((0, 1))
+    snake_list.append((0, 0))
+    game.data['snake_list'] = snake_list
+
+    main_func()
+
+
+def create_apple():
+    map_size = game.data['map']['map_size']
+
+    # 随机苹果的位置，当位置与蛇重合的时候，重新随机
+    import random
+    apple_position = (random.randint(0, map_size - 1), random.randint(0, map_size - 1))
+    while apple_position in game.data['snake_list']:
+        apple_position = (random.randint(0, map_size - 1), random.randint(0, map_size - 1))
+
+    # 将mapdata中的对应苹果位置改为1
+    game.data['mapdata'][apple_position] = 1
+
+
+def draw_map():
+    map_size = game.data['map']['map_size']
+    snake_list = game.data['snake_list']
+    mapdata = game.data['mapdata']
+    for y in range(0, map_size):
+        for x in range(0, map_size):
+            pos = (x, y)
+            if pos in snake_list:
+                game.p('❖', style='special')
+                continue
+            if mapdata[pos] == 1:
+                game.p('❁')
+                continue
+            if mapdata[pos] == 0:
+                game.p('﹒')
+                continue
+        game.pl()
+
+
+direction = 'xia'
+
+def next_step():
+    # 方便调用
+    global direction
+    snake_list = game.data['snake_list']
+    mapdata = game.data['mapdata']
+
+    # 判断是否有苹果，没有就创造一个
+    if 1 not in mapdata.values():
+        create_apple()
+
+    # 更新蛇身
+    for i in range(len(snake_list) - 1, 0, -1):
+        snake_list[i] = snake_list[i - 1]
+
+    # 更新蛇头位置
+    x_head = snake_list[0][0]
+    y_head = snake_list[0][1]
+    if direction == 'zuo':
+        x_head = x_head - 1
+    if direction == 'you':
+        x_head = x_head + 1
+    if direction == 'shang':
+        y_head = y_head - 1
+    if direction == 'xia':
+        y_head = y_head + 1
+    snake_list[0] = (x_head, y_head)
 
 def main_func():
+    # 新界面准备
     game.clr_cmd()
-    game.pl()
     game.pline()
-    game.pl('主神名称：' + game.data['主神名称'])
-    import script.play_cfg
-    script.play_cfg.display_traget()
+
+    # 画图
+    draw_map()
+
+    def create_func(direction_name):
+        def func():
+            global direction
+            direction = direction_name
+            next_step()
+            main_func()
+        return func
+
+    # 状态显示
     game.pline('--')
+    game.pl('分数：')
 
-    import script.play
-    game.pcmd('[101]  开始试炼', 101, script.play.init_play)
-    game.p('    ')
-    import script.play_cfg
-    game.pcmd('[102]  试炼设置', 102, script.play_cfg.play_config)
+    # 绘制命令按钮
+    game.pline('--')
+    game.pcmd('[1] 左  ', 1, create_func('zuo'))
+    game.pcmd('[2] 上  ', 2, create_func('shang'))
+    game.pcmd('[3] 右  ', 3, create_func('you'))
     game.pl()
-    import script.people
-    game.pcmd('[103]  人物管理', 103, script.people.people_manager)
-    game.p('    ')
-    import script.group
-    game.pcmd('[104]  队伍管理', 104, script.group.group_manager)
-    game.p('    ')
-    import script.world
-    game.pcmd('[105]  世界管理', 105, script.world.world_manager)
-    game.pl()
-    game.pcmd('[111]  保存游戏', 111, saveload.save_func)
-    game.p('    ')
-    game.pcmd('[112]  读取游戏', 112, saveload.load_func)
-
+    game.p('        ')
+    game.pcmd('[4] 下  ', 4, create_func('xia'))
+    game.pl('\n')
+    game.pcmd('[5] 存储游戏  ', 5, lib.save_func, arg=main_func)
+    game.pcmd('[6] 读取游戏  ', 6, lib.load_func, arg=(main_func, main_func))
